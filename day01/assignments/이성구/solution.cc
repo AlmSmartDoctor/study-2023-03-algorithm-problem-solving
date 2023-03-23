3ce7dfe1c016564ea5f791ce54343c741c0d31a5
TwoCastle9 <37656369+uuccu@users.noreply.github.com>
2023-03-23 17:50:02 +0900

    가르침 문제 품

---
+++ day01/assignments/이성구/solution.cc
@@ -0,0 +1,53 @@
+int ret = 0;
+
+void recursive(int n, int k, int toPick, int checked, int start,vector<int>& words) {
+
+    if (toPick == 0) {
+        int cnt = 0;
+        for (int i = 0; i < n; i++){
+            if((words[i] & checked) == words[i])cnt++;
+        }
+        if(ret < cnt)ret = cnt;
+        return;
+    }
+    for (int i = start; i < 26; i++) {
+        if (!(checked & (1 << i))) {
+            checked |= (1 << i);
+            recursive(n, k, toPick - 1, checked, i,words);
+            checked = checked & ~(1 << i);
+        }
+    }
+
+}
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> words(n);
+    vector<string> antaticaWords(n);
+    int cnt = 5, i = 0;
+
+    for (auto &antaticaWord: antaticaWords) {
+        cin >> antaticaWord;
+        int alphabets = 0;
+
+        for (auto alphabet: antaticaWord)
+            alphabets |= 1 << (alphabet - 'a');
+
+        words[i++] = alphabets;
+    }
+    int checked = 0;
+    checked |= 1 << ('a' - 'a');
+    checked |= 1 << ('n' - 'a');
+    checked |= 1 << ('t' - 'a');
+    checked |= 1 << ('i' - 'a');
+    checked |= 1 << ('c' - 'a');
+
+    if (k < 5) cout<< 0;
+    else if (k == 26) cout<< n;
+    else {
+        recursive(n, k, k - 5, checked, 0,words);
+        cout<<ret;
+    }
+}
