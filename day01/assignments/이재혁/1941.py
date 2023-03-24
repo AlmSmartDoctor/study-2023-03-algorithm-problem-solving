c457c133ddd76240b738103377814bfff2f720d5
HYEOK9 <leejaehyuck9@gmail.com>
2023-03-24 10:09:21 +0900

    add annotation

--- day01/assignments/이재혁/1941.py
+++ day01/assignments/이재혁/1941.py
@@ -1,33 +1,34 @@
 import sys
 input=sys.stdin.readline
-from collections import deque
 
+# 좌석표
 graph=[list(input().strip()) for _ in range(5)]
 dx,dy=[0,0,-1,1],[1,-1,0,0]
-visited=set()
+ansList=set()
 
-ans=0
-
-def checkRange(x,y):
+def checkRange(y,x):
   return 0<=x<5 and 0<=y<5
 
 def dfs(picked,S,Y):
-  global ans,visited
+  global ansList
+  # 임도연파가 3명이상이면 할 필요 없음
   if(Y>3):
     return
   
+  # 정답을 만족하는 경우
   if len(picked) == 7 and S >= 4:
     picked=tuple(sorted(picked))
-    if picked not in visited:
-      visited.add(picked)
-      ans+=1
+    if picked not in ansList:
+      ansList.add(picked)
     return
-  
+    
   for pos in picked:
+        # 5로 나눈 몫, 나머지 => 좌표 값
         y=pos//5
         x=pos%5
         for i in range(4):
             ny,nx=y+dy[i],x+dx[i]
+            # n=좌석 번호
             n=ny*5+nx
             if checkRange(ny, nx) and n not in picked:
                 picked.append(n)
@@ -37,6 +38,7 @@ def dfs(picked,S,Y):
                     dfs(picked, S+1, Y)
                 picked.pop()
 
+# run
 for i in range(5):
     for j in range(5):
         if graph[i][j] == 'S':
@@ -45,4 +47,4 @@ for i in range(5):
             dfs(picked, 1, 0)
             
             
-print(ans)
+print(len(ansList))
