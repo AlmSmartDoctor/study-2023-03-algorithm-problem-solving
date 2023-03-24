c14f3bed5f862fefc00862838849b7618244edca
Hajin Kim <kimhajin@yonsei.ac.kr>
2023-03-24 09:15:57 +0900

    결국 해결

--- day01/assignments/김하진/Main.kts
+++ day01/assignments/김하진/Main.kts
@@ -1,33 +1,30 @@
-// 현재 상태: BFS를 안 쓰고 특이하게 풀다가 틀림
+inline fun <T, reified R> Array<Array<T>>.map2(f: (T) -> R): Array<Array<R>> =
+    map { it.map(f).toTypedArray() }.toTypedArray()
 
-fun <T, R> List<List<T>>.map2(f: (T) -> R): List<List<R>> = map { it.map(f) }
-
-fun <K, V> emptyMutableMap() = emptyMap<K, V>().toMutableMap()
-
-fun List<List<Int>>.isSafeFromJangma(gansuryang: Int): List<List<Boolean>> =
+fun Array<Array<Int>>.isSafeFromJangma(gansuryang: Int): Array<Array<Boolean>> =
     map2 { it > gansuryang }
 
-fun List<List<Boolean>>.getNumberOfSafeZones(): Int {
-    var numberOfSafeZones = 0
-    val isCountedMap = emptyMap<Int, MutableMap<Int, Boolean>>().toMutableMap()
-
-    isCountedMap[-1] = emptyMutableMap<Int, Boolean>()
-    forEachIndexed { index, _ ->
-        isCountedMap[index] = emptyMutableMap<Int, Boolean>()
+fun Array<Array<Boolean>>.recursivelyMarkFalse(x: Int, y: Int) {
+    if (x < 0 || y < 0 || x >= size || y >= size) {
+        return
     }
-    isCountedMap[size] = emptyMutableMap<Int, Boolean>()
+    if (this[y][x] == false) return
+
+    this[y][x] = false
+    recursivelyMarkFalse(x = x + 1, y = y)
+    recursivelyMarkFalse(x = x - 1, y = y)
+    recursivelyMarkFalse(x = x, y = y + 1)
+    recursivelyMarkFalse(x = x, y = y - 1)
+}
+
+fun Array<Array<Boolean>>.getNumberOfSafeZones(): Int {
+    var numberOfSafeZones = 0
 
     mapIndexed { y, xs ->
         xs.mapIndexed { x, isSafe ->
             if (isSafe) {
-                if (isCountedMap[y]!![x] != true) {
-                    numberOfSafeZones++
-                }
-                isCountedMap[y]!![x] = true
-                isCountedMap[y]!![x + 1] = true
-                isCountedMap[y]!![x - 1] = true
-                isCountedMap[y + 1]!![x] = true
-                isCountedMap[y - 1]!![x] = true
+                numberOfSafeZones++
+                recursivelyMarkFalse(x = x, y = y)
             }
         }
     }
@@ -35,17 +32,21 @@ fun List<List<Boolean>>.getNumberOfSafeZones(): Int {
     return numberOfSafeZones
 }
 
-val size = readln().toInt()
-val zone = (1..size).map { readln().split(" ").map { it.toInt() } }
+fun main() {
+    val size = readln().toInt()
+    val zone = (1..size).map { readln().split(" ").map { it.toInt() }.toTypedArray() }.toTypedArray()
 
-val maxGansuryang = zone.flatten().max()
+    val maxGansuryang = zone.maxOfOrNull { it.max() }!!
 
-val gansuryangs = 0..maxGansuryang
+    val gansuryangs = 0..maxGansuryang
 
-val maxNumberOfSafeZones = gansuryangs.maxOfOrNull { gansuryang ->
-    zone
-        .isSafeFromJangma(gansuryang)
-        .getNumberOfSafeZones()
-} ?: 0
+    val maxNumberOfSafeZones = gansuryangs.maxOfOrNull { gansuryang ->
+        zone
+            .isSafeFromJangma(gansuryang)
+            .getNumberOfSafeZones()
+    } ?: 0
 
-println(maxNumberOfSafeZones)
+    println(maxNumberOfSafeZones)
+}
+
+main() // Kotlin Script
