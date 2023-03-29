6a62b81caee3f3fe4b60ca1c75c2b76ba295f2b5
Jaeheon Kim <48355679+xhdtlsid2@users.noreply.github.com>
2023-03-29 21:23:33 +0900

    출제자의 의도 파악

--- day02 ch19/assignments/김재헌/14035.cpp
+++ day02 ch19/assignments/김재헌/14035.cpp
@@ -1,90 +1,45 @@
 #include "bits/stdc++.h"
 using namespace std;
 
-typedef long long ll;
-
 const string s = "NESW";
-
 const int dy[] = {-1, 0, 1, 0};
 const int dx[] = {0, 1, 0, -1};
 
-struct Myset{
-    vector<int> p, out;
-
-    Myset(int n){
-        p.resize(n, -1);
-        out.resize(n);
-    }
-
-    int find(int x){
-        if(p[x] < 0) return x;
-        return p[x] = find(p[x]);
-    }
-
-    void merge(int u, int v){
-        u = find(u);
-        v = find(v);
-        if(u == v) return;
-        p[u] = v;
-    }
-};
-
 int main(){
-    ios::sync_with_stdio(0); cin.tie(0);
-    int n, m;
-    cin >> n >> m;
-    vector<string> v(n);
-    for(int i = 0; i < n; i++){
-        cin >> v[i];
-    }
-    vector<Myset> nxt(4, Myset(n * m));
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < m; j++){
-            if(v[i][j] != '.') continue;
-            for(int k = 0; k < 4; k++){
-                int ny = i + dy[k], nx = j + dx[k];
-                if(ny < 0 || ny >= n || nx < 0 || nx >= m){
-                    nxt[k].out[i * m + j] = 1;
-                }
-                else{
-                    nxt[k].merge(i * m + j, ny * m + nx);
-                }
-            }
-        }
-    }
-    queue<pair<int, int> > q;
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < m; j++){
-            if(v[i][j] == '.') continue;
-            int d = s.find(v[i][j]);
-            int ny = i + dy[d], nx = j + dx[d];
-            if(ny < 0 || ny >= n || nx < 0 || nx >= m || nxt[d].out[nxt[d].find(ny * m + nx)]){
-                q.push({i, j});
-            }
-        }
-    }
-    while(q.size()){
-        int y, x;
-        tie(y, x) = q.front();
-        q.pop();
+  ios::sync_with_stdio(0); cin.tie(0);
+  int n, m;
+  cin >> n >> m;
+  vector<string> v(n);
+  for(int i = 0; i < n; i++){
+    cin >> v[i];
+  }
+  vector<vector<bool> > mark(n, vector<bool>(m));
+  function<void(int, int)> solve = [&](int y, int x){
+    int d = s.find(v[y][x]);
+    int ty = y, tx = x;
+    while(1){
+      ty += dy[d];
+      tx += dx[d];
+      if(ty < 0 || ty >= n || tx < 0 || tx >= m || (mark[ty][tx] && v[y][x] == v[ty][tx])){
+        mark[y][x] = 1;
         cout << '(' << y << ',' << x << ')' << '\n';
-        for(int k = 0; k < 4; k++){
-            int ny = y + dy[k], nx = x + dx[k];
-            if(ny < 0 || ny >= n || nx < 0 || nx >= m){
-                nxt[k].out[y * m + x] = 1;
-            }
-            else{
-                nxt[k].merge(y * m + x, ny * m + nx);
-            }
+        return;
+      }
+      if(v[ty][tx] != '.' && !mark[ty][tx]){
+        solve(ty, tx);
+        if(v[y][x] == v[ty][tx]){
+          mark[y][x] = 1;
+          cout << '(' << y << ',' << x << ')' << '\n';
+          return;
         }
-        for(int k = 0; k < 4; k++){
-            int p = nxt[k].find(y * m + x);
-            if(nxt[k].out[p]) continue;
-            int od = (k + 2) % 4;
-            if(s.find(v[p / m][p % m]) != od) continue;
-            int py = p / m, px = p % m;
-            if(!nxt[od].out[nxt[od].find((py + dy[od]) * m + (px + dx[od]))]) continue;
-            q.push({py, px});
-        }
+      }
     }
+  };
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j < m; j++){
+      if(v[i][j] != '.' && !mark[i][j]){
+        solve(i, j);
+      }
+    }
+  }
 }
