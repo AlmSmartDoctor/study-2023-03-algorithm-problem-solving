afae9c8591737b80cf492a3079049ca037568c66
Hajin Kim <kimhajin@yonsei.ac.kr>
2023-04-14 10:12:43 +0900

    Update Main.kts

--- day04 Greedy/assignments/김하진/Main.kts
+++ day04 Greedy/assignments/김하진/Main.kts
@@ -4,6 +4,7 @@
 // 1. 단순 카운팅으로 풀 수 있을까 -> 실패
 // 2. (현재) 선형 탐색으로 풀 수 있을까 -> 실패
 //   - bbaaaaaaaaaaaaaaacccccc일 때 delete c and replace c -> delete two a and replace four a 를 처리하기 어려움
+//   - 시도할 예정인 것: 상수 횟수의 선형 탐색으로 해결 가능할지
 class Solution {
     fun strongPasswordChecker(password: String): Int {
         var requiredInsertion = maxOf(6 - password.length, 0)
