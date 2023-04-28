f8db13e60ffc11df7c121bc7f210d67e8078e801
HYEOK9 <leejaehyuck9@gmail.com>
2023-04-28 09:42:23 +0900

    annotation

--- day06 dp techniques/assignment/이재혁/4811.py
+++ day06 dp techniques/assignment/이재혁/4811.py
@@ -12,16 +12,18 @@ while n != 0:
 
 def solve(n):
     cache = [[0] * (n + 1) for _ in range(n + 1)]
+    # 남은 약이 다 반개 짜리면 경우의 수 = 1
     for i in range(n + 1):
         cache[0][i] = 1
 
     for i in range(1, n + 1):
-        for j in range(i, n + 1):
-            cache[i][j] = cache[i - 1][j] + cache[i][j - 1]
+        for j in range(n):
+            cache[i][j] = cache[i - 1][j + 1] + cache[i][j - 1]
 
-    return cache[n][n]
+    # cache[n][0] == cache[n-1][1]
+    return cache[n][0]
 
-
+# run
 if __name__ == "__main__":
     for i in lst:
         print(solve(i))
