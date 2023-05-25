b3d6f0941028c24a7829c0f4e695613e477cb79d
박진영 <tear4321@cau.ac.kr>
2023-05-25 20:52:51 +0900

    풀이/pq

---
+++ day10 pq/박진영/Solution.kt
@@ -0,0 +1,44 @@
+// https://leetcode.com/problems/kth-largest-element-in-a-stream/description/
+/**
+ * 최초시도 -> bruteforce(
+ *  - 3이상이면 대충 넣고
+ *  - 아니면 제대로 넣고
+class KthLargest(k: Int, nums: IntArray) {
+
+private val zerobaseK = k - 1
+private val sortedMutableList: MutableList<Int> = nums.sortedDescending().toMutableList()
+
+fun add(`val`: Int): Int {
+for (i in 0 .. sortedMutableList.size) {
+if (i > zerobaseK || i == sortedMutableList.size) {
+sortedMutableList.add(`val`)
+break
+}
+if (sortedMutableList[i] < `val`) {
+sortedMutableList.add(i, `val`)
+break
+}
+}
+return sortedMutableList[zerobaseK]
+}
+
+}
+ * */
+class KthLargest(private val k: Int, nums: IntArray) {
+
+    private val pq = PriorityQueue<Int>()
+
+    init {
+        pq.addAll(nums.asIterable())
+        while(pq.size> k) {
+            pq.poll()
+        }
+    }
+
+    fun add(`val` : Int) : Int {
+        pq.add(`val`)
+        pq.poll()
+        return pq.first()
+    }
+
+}
